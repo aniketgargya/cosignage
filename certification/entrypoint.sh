9208ab594ceddb650868ed42c86dcafdcb0d1938
Aniket Gargya <gargya.aniket@gmail.com>
2020-08-18 17:08:53 -0500

    good env

--- certification/entrypoint.sh
+++ certification/entrypoint.sh
@@ -8,18 +8,18 @@ then
         --noninteractive \
 		--webroot \
 		--webroot-path /var/www/certbot \
-        -d encrypt2.omnistacks.com
+        -d "$DOMAIN"
 
     mkdir -p /etc/letsencrypt/certificates/c
 
-    cp -r /etc/letsencrypt/live/encrypt2.omnistacks.com/* /etc/letsencrypt/certificates/c
+    cp -r /etc/letsencrypt/live/"$DOMAIN"/* /etc/letsencrypt/certificates/c
     touch /etc/letsencrypt/certificates/c/certbot.txt
 fi
 
 while true; do
     echo "Renewing certificates";
     /usr/local/bin/certbot-auto renew;
-    cp -r /etc/letsencrypt/live/encrypt2.omnistacks.com/* /etc/letsencrypt/certificates/c
+    cp -r /etc/letsencrypt/live/"$DOMAIN"/* /etc/letsencrypt/certificates/c
 
     sleep 604800;
 done
--- proxy/entrypoint.sh
+++ proxy/entrypoint.sh
@@ -1,9 +1,9 @@
-if [ ! -f /etc/letsencrypt/live/encrypt2.omnistacks.com/privkey.pem ];
+if [ ! -f /etc/letsencrypt/live/"$DOMAIN"/privkey.pem ];
 then
     echo "Generating dummy certificates"
     mkdir -p /etc/letsencrypt/certificates/c
     openssl genrsa -out /etc/letsencrypt/certificates/c/privkey.pem 4096
-    openssl req -new -key /etc/letsencrypt/certificates/c/privkey.pem  -out /etc/letsencrypt/certificates/c/cert.csr -nodes -subj "/C=PT/ST=World/L=World/O=encrypt2.omnistacks.com/OU=ilhicas lda/CN=encrypt2.omnistacks.com/EMAIL=gargya.aniket@gmail.com"
+    openssl req -new -key /etc/letsencrypt/certificates/c/privkey.pem  -out /etc/letsencrypt/certificates/c/cert.csr -nodes -subj "/C=PT/ST=World/L=World/O=$DOMAIN/OU=ilhicas lda/CN=$DOMAIN/EMAIL=gargya.aniket@gmail.com"
     openssl x509 -req -days 365 -in /etc/letsencrypt/certificates/c/cert.csr -signkey /etc/letsencrypt/certificates/c/privkey.pem -out /etc/letsencrypt/certificates/c/fullchain.pem
 fi
 
