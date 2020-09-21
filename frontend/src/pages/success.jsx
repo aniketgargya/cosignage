ba89d740469eb3632f2fafb6c3c395308a89400e
Aniket Gargya <gargya.aniket@gmail.com>
2020-09-20 22:14:19 -0500

    success page

--- frontend/src/pages/success.jsx
+++ frontend/src/pages/success.jsx
@@ -1,11 +1,25 @@
 import { useEffect } from "react";
+import style from "../styles/pages/success.css";
+import Head from "next/head";
 
 const Success = () => {
     useEffect(() => {
         localStorage.setItem("cart", JSON.stringify({}));
     });
 
-    return <p>You've successfully purchased your items!</p>;
+    return (
+        <>
+            <style jsx>{style}</style>
+            <Head>
+                <title>Success | cosignage.info</title>
+            </Head>
+
+            <main>
+                <h1>You've successfully purchased your items!</h1>
+                <p>Thank you for shopping at Cosignage</p>
+            </main>
+        </>
+    );
 };
 
 export default Success;
